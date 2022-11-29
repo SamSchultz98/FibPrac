21ca946fc9e77fd36f84663f87d2598c0c6756c1
SamSchultz98 <sam.schultz@outlook.com>
2022-11-29 15:59:20 -0500

    Add project files.

---
+++ FibPrac/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+
+
+
+
+
+
+
+
+
+Fib(3, 3000);
+
+
+
+
+
+static int Fib(int i)
+{
+
+    while (i < 1000)
+    {
+        int k = i;
+        int tot = i + k;
+        Console.WriteLine(tot);
+        k = tot + 1;
+        i = tot;
+    }
+    Console.WriteLine("Fib Done");
+}
+
